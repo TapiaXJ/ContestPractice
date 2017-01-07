8c878f21976ad922fb60cde9863f9d59eb3f4eb9
Jonathan Tapia <tapiaxj@gmail.com>
2017-01-06 17:01:56 -0800

    Initial commit

---
+++ Code/FactorialFactors.cpp
@@ -0,0 +1,51 @@
+/*
+Algorithm:
+n!
+
+int result
+
+find prime numbers between 2 and n //Sieve of Eratosthenes
+
+for each prime number p
+	for 1 .. logp(n)
+		result += n/(p^i)
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+    int result = 0;
+
+    cout << "Enter number: ";
+    cin >> n;
+    bool primes[n+1];
+
+    for(int i = 0; i < n+1; i++){
+        primes[i] = true;
+    }
+
+    primes[0] = false;
+    primes[1] = false;
+
+    int p = 2;
+
+    while(p <= n){
+        for(int i = p+p; i < n + 1; i += p){
+            primes[i] = false;
+        }
+
+        int maxI = log(n)/log(p);
+        for(int i = 1; i < maxI + 1; i++){
+            result += n/(pow(p, i));
+        }
+
+        do{
+            p++;
+        } while(primes[p] == false);
+    }
+
+    cout << "Result is: " << result << endl;
+}
